1e1620d1b856a01b0dc3e2b41ef3b1bdaf1bab3b
York <york.tsai@fundroots.com>
2019-12-26 10:43:41 +0800

    no message

---
+++ src/test/java/leetCode/LeetCode038.java
@@ -0,0 +1,79 @@
+package leetCode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LeetCode038 {
+
+
+
+    private String countAndSay(int times) {
+
+       String result = "1";
+
+       while (times > 1) {
+           result = doCountAndSay(result);
+           times--;
+       }
+
+        return result;
+
+    }
+
+    private String doCountAndSay(String s) {
+        StringBuilder sb = new StringBuilder();
+        int count = 1;
+        char say = s.charAt(0);
+
+        for (int i = 1; i < s.length(); i++) {
+
+            if (s.charAt(i) == say) {
+                count++;
+            } else {
+                sb.append(count).append(say);
+                say = s.charAt(i);
+                count = 1;
+            }
+        }
+
+        sb.append(count).append(say);
+        return sb.toString();
+    }
+
+    @Test
+    public void test1() {
+        String result = this.countAndSay(1);
+        assertEquals("1", result);
+    }
+
+    @Test
+    public void test2() {
+        String result = this.countAndSay(2);
+        assertEquals("11", result);
+    }
+
+    @Test
+    public void test3() {
+        String result = this.countAndSay(3);
+        assertEquals("21", result);
+    }
+
+    @Test
+    public void test4() {
+        String result = this.countAndSay(4);
+        assertEquals("1211", result);
+    }
+
+    @Test
+    public void test5() {
+        String result = this.countAndSay(5);
+        assertEquals("111221", result);
+    }
+
+    @Test
+    public void test6() {
+        String result = this.countAndSay(6);
+        assertEquals("312211", result);
+    }
+}
---
+++ src/test/java/leetCode/LeetCode058.java
@@ -0,0 +1,41 @@
+package leetCode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LeetCode058 {
+
+    public int lengthOfLastWord(String s) {
+
+        String[] splitString = s.split(" ");
+
+        if (splitString.length == 0) {
+            return 0;
+        }
+
+        return splitString[splitString.length - 1].length();
+    }
+
+    @Test
+    public void test1() {
+        String str = "Hello World";
+
+        assertEquals(5, lengthOfLastWord(str));
+    }
+
+    @Test
+    public void test2() {
+        String str = "";
+
+        assertEquals(0, lengthOfLastWord(str));
+    }
+
+    @Test
+    public void test3() {
+        String str = "Hello";
+
+        assertEquals(5, lengthOfLastWord(str));
+    }
+
+}
